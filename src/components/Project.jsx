3a7613f8d20c063e01a73a16c68ab030cc38f4ec
mubarak-codes <mubarakcodes@gmail.com>
2025-04-16 20:46:35 +0100

    fixed border and resume button

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,7 +13,7 @@ const Project = ({bg, text,})=>{
     <h2 className="text-center ff-rubik mb-4 mfs-9">PROJECT</h2>
     <div className=" gap-4 d-grid gr-2-sm">
      {projects.map((project)=>(
-      <div className={`bg-success bg-opacity-10 border border-success border-opacity-50 rounded-4  d-flex p-3  ${text}`} key={project.id}>
+      <div className={`bg-success bg-opacity-10 border border-2 border-success border-opacity-50 rounded-4  d-flex p-3  ${text}`} key={project.id}>
        <div className="">
         <h4 className="ff-montserrat-bold mb-2 mfs-4">{project.title}</h4>
         <div className="mb-2 mfs-2 opacity-75">{project.description}</div>
