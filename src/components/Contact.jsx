a278bb661f90d4e93a635cf42940a0bf56684f87
mubarak-codes <mubarakcodes@gmail.com>
2025-04-28 09:49:48 +0100

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,10 +8,12 @@ const Contact = ({text})=>{
    <section className="mt12" id="contact">
     <h3 className="ff-rubik fs9 mb7 text-center">CONTACT</h3>
     <div className="text-center mb4 opacity75 fs2">Let's build something amazing together</div>
-    <div className="flex content-center fs7 gr-auto gap-4">
-     <a href="mailto:mubarakcodes@gmail.com" className={`${text}`} target="_blank"><i className={`fa-solid fa-envelope click-fade click-grow`}></i></a>
-     <a href="https://wa.me/+2347062902419" className={`${text}`} target="_blank"><i class="fa-brands fa-whatsapp click-grow click-fade"></i></a>
-     <a href="tel:+2347062902419" className={`${text}`} target="_blank"><i class="fa-solid fa-phone click-fade click-grow"></i></a>
+    <div className="flex d-col gap-3 items-center fs7 opacity75">
+     <div className="flex gap-3 d-col d-row-sm content-center">
+      <a href="mailto:mubarakcodes@gmail.com" className={`${text} cursor-pointer fs2 b b-green p1 px2`} target="_blank"><i className={`fa-solid fa-envelope hover-fade click-grow me2 fs4 vertical-center`}></i>mubarakcodes@gmail.com</a>
+      <a href="https://wa.me/+2347062902419" className={`${text} cursor-pointer fs2 b b-green p1 px2 text-center mx9 mx1-sm`} target="_blank"><i class="fa-brands fa-whatsapp click-grow hover-fade me2 fs4 vertical-center"></i>+2347062902419</a>
+     </div>
+     <a href="tel:+2347062902419" className={`${text} cursor-pointer fs2 b b-green p1 px2`} target="_blank"><i class="fa-solid fa-phone hover-fade click-grow me2 fs4 vertical-center"></i>+2347062902419</a>
     </div>
    </section>
   </>
