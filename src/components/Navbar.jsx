fa783989e50de387e7c40de3e4d75981681fe681
mubarak-codes <mubarakcodes@gmail.com>
2025-04-28 14:24:19 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 
 
-
 const Navbar = ({text, bg})=>{
 
  return(
@@ -10,21 +9,13 @@ const Navbar = ({text, bg})=>{
      <div className="navbar-brand fw-bolder container-fluid ff-rubik text-uppercase text-green fs5">
           Mubarak
      </div>
-     <i data-mk-target="#navbar" className="fa fa-bars offscreen-toggler"></i>
-     <ul id="navbar" className="navbar-nav me3 gap-3 fs2 offscreen-start">
-      <li className="nav-item cursor-pointer hover-fade"><a href="#about" className={``}>About</a></li>
+     <i data-mk-target="#navbar" className="fa d-none fa-bars offscreen-toggler"></i>
+     <ul id="navbar" className="navbar-nav me3 gap-3 fs2 offscreen-star">
+      <li className="nav-item cursor-pointer hover-fade"><a href="#about" className={`d-none`}>About</a></li>
       <li className="nav-item cursor-pointer hover-fade"><a href="#projects" className={``}>Projects</a></li>
-      <li className="nav-item cursor-pointer hover-fade"><a href="#experience" className={``}>Experience</a></li>
+      <li className="nav-item cursor-pointer hover-fade"><a href="#experience" className={`d-none`}>Experience</a></li>
       <li className="nav-item cursor-pointer hover-fade"><a href="#contact" className={``}>Contact</a></li>
      </ul>
-     <div data-mk-target="#end" class="offscreen-toggler bg-green text-snow ">draw end</div>
-
-<div id="end" class="offscreen-end w60 h50 text-ebony b b-green rounded-3  bg-cyan mx4">
- 
-  <div class="">Johnny </div><a href="#">#</a>
-  <div>hello </div>
-  <div>Yaba </div>
-</div>
     </nav>
    </section>
 
