236562aa0c5dd886c8664e9cf888aa0ebe71ac77
mubarak-codes <mubarakcodes@gmail.com>
2025-04-29 15:42:55 +0100

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,14 +8,14 @@ const Skills = ()=>{
    <section className=" mt12 px4 px10-sm px20-md">
     <h2 className="fs8 ff-rubik mb8 text-center">SKILLS</h2>
     <div className="d-grid  ff-montserrat gr2 gr3-sm gr4-lg fw-bolder gap-3">
-     <div className="py4 bg-gray10 px3 hover-grow hover-text-green transit"><i className="fab fa-html5 me3 fs9 vertical-center"></i>HTML</div>
-     <div className=" py4 bg-gray10 px3 hover-grow hover-text-green transit"><i className="fab fa-css3-alt me3 fs9 vertical-center"></i>CSS</div>    
-     <div className=" py4 bg-gray10 px3 hover-grow hover-text-green transit"><i className="fab fa-js me3 fs9 vertical-center"></i>JavaScript</div>
-     <div className=" py4 bg-gray10 px3 hover-grow hover-text-green transit"><i className="fab fa-bootstrap me3 fs9 vertical-center"></i>Bootstrap</div>
-     <div className=" py4 bg-gray10 px3 hover-grow hover-text-green transit"><i className="fab fa-react me3 fs9 vertical-center"></i>React</div>
-     <div className=" py4 bg-gray10 px3 hover-grow hover-text-green transit"><i className="fab fa-git-alt me3 fs9 vertical-center"></i>Git</div>
-     <div className=" py4 bg-gray10 px3 hover-grow hover-text-green transit"><i className="fab fa-github me3 fs9 vertical-center"></i>Github</div>
-     <div className=" py4 bg-gray10 px3 hover-grow hover-text-green transit"><i className="fab fa-html5 me3 fs9 vertical-center"></i>MkCSS</div>
+     <div className="py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-html5 me3 fs9 vertical-center"></i>HTML</div>
+     <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-css3-alt me3 fs9 vertical-center"></i>CSS</div>    
+     <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-js me3 fs9 vertical-center"></i>JavaScript</div>
+     <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-bootstrap me3 fs9 vertical-center"></i>Bootstrap</div>
+     <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-react me3 fs9 vertical-center"></i>React</div>
+     <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-git-alt me3 fs9 vertical-center"></i>Git</div>
+     <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-github me3 fs9 vertical-center"></i>Github</div>
+     <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-html5 me3 fs9 vertical-center"></i>MkCSS</div>
 
     </div>
    </section>
