dbef0c3235e57303555c0958c8104fdfeedf448e
mubarak-codes <mubarakcodes@gmail.com>
2025-04-29 11:51:29 +0100

    navbar

--- external-library/mkCss/js/mk-css-offscreen.js
+++ external-library/mkCss/js/mk-css-offscreen.js
@@ -20,7 +20,7 @@ let overlayHide = document.querySelector('.overlay-hide');
   offscreenTarget.classList.add('offscreen-false-top')
  } else if (offscreenTarget.classList.contains('offscreen-start')) {
  offscreenTarget.classList.add('offscreen-false-start')
- } else if (offscreenTarget.classList.contains('offscreen-end') || offscreenTarget.classList.contains('offscreen-end-sm')) {
+ } else if (offscreenTarget.classList.contains('offscreen-end')) {
  offscreenTarget.classList.add('offscreen-false-end')
  }
  
@@ -39,7 +39,7 @@ let overlayHide = document.querySelector('.overlay-hide');
    } else if (offscreenTarget.classList.contains('offscreen-start')) {
     offscreenTarget.classList.remove('offscreen-false-start');
     offscreenTarget.classList.add('offscreen-true-start');
-   } else if (offscreenTarget.classList.contains('offscreen-end') || offscreenTarget.classList.contains('offscreen-end-sm')) {
+   } else if (offscreenTarget.classList.contains('offscreen-end')) {
     offscreenTarget.classList.remove('offscreen-false-end');
     offscreenTarget.classList.add('offscreen-true-end');
    }
@@ -55,7 +55,7 @@ let overlayHide = document.querySelector('.overlay-hide');
    } else if (offscreenTarget.classList.contains('offscreen-start')){
     offscreenTarget.classList.remove('offscreen-true-start');
     offscreenTarget.classList.add('offscreen-false-start');
-   } else if (offscreenTarget.classList.contains('offscreen-end')|| offscreenTarget.classList.contains('offscreen-end-sm')){
+   } else if (offscreenTarget.classList.contains('offscreen-end')){
     offscreenTarget.classList.remove('offscreen-true-end');
     offscreenTarget.classList.add('offscreen-false-end');
    }
@@ -77,7 +77,7 @@ let overlayHide = document.querySelector('.overlay-hide');
    } else if (offscreenTarget.classList.contains('offscreen-start')){
     offscreenTarget.classList.remove('offscreen-true-start');
     offscreenTarget.classList.add('offscreen-false-start');
-   } else if (offscreenTarget.classList.contains('offscreen-end')|| offscreenTarget.classList.contains('offscreen-end-sm')){
+   } else if (offscreenTarget.classList.contains('offscreen-end')){
     offscreenTarget.classList.remove('offscreen-true-end');
     offscreenTarget.classList.add('offscreen-false-end');
    } 
@@ -99,7 +99,7 @@ let overlayHide = document.querySelector('.overlay-hide');
    } else if (offscreenTarget.classList.contains('offscreen-start')){
     offscreenTarget.classList.remove('offscreen-true-start');
     offscreenTarget.classList.add('offscreen-false-start');
-   } else if (offscreenTarget.classList.contains('offscreen-end')|| offscreenTarget.classList.contains('offscreen-end-sm')){
+   } else if (offscreenTarget.classList.contains('offscreen-end')){
     offscreenTarget.classList.remove('offscreen-true-end');
     offscreenTarget.classList.add('offscreen-false-end');
    }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,22 @@ const Navbar = ({text, bg})=>{
  return(
   <>
    <section className=" mb5">
-    <nav className={`navbar px13-sm px15-md navbar-expand ${bg} fixed py3 left0 right0 top0 z-index100`}>
-     <div className="navbar-brand fw-bolder container-fluid ff-rubik text-uppercase text-green fs5">
+    <nav className={`navbar navbar-expand-sm ${bg} fixed-top py3 left0  right0 top0 `}>
+     <div className=" container ">
+      <div className=" fw-bolder ff-rubik text-uppercase text-green fs5">
           Mubarak
+      </div>
+      <i role="button" aria-controls="offcanvasExample" data-bs-target="#navbar" data-bs-toggle="collapse" className="fa fa-bars d-inline d-sm-none fs6"></i>
+
+      <div id="navbar" className="collapse navbar-collapse">
+       <ul className="flex fs2 gap-3 navbar-nav py3 ms-auto">
+         <li className="nav-item cursor-pointer hover-fade"><a href="#about" className={`link-inherit`}>About</a></li>
+         <li className="nav-item cursor-pointer hover-fade"><a href="#projects" className={`link-inherit`}>Projects</a></li>
+         <li className="nav-item cursor-pointer hover-fade"><a href="#experience" className={`link-inherit`}>Experience</a></li>
+         <li className="nav-item cursor-pointer hover-fade"><a href="#contact" className={`link-inherit`}>Contact</a></li>
+       </ul> 
+      </div>
      </div>
-     <i data-mk-target="#navbar" className="fa d-none fa-bars offscreen-toggler"></i>
-     <ul id="navbar" className="navbar-nav me3 gap-3 fs2 offscreen-star">
-      <li className="nav-item cursor-pointer hover-fade"><a href="#about" className={`d-none`}>About</a></li>
-      <li className="nav-item cursor-pointer hover-fade"><a href="#projects" className={``}>Projects</a></li>
-      <li className="nav-item cursor-pointer hover-fade"><a href="#experience" className={`d-none`}>Experience</a></li>
-      <li className="nav-item cursor-pointer hover-fade"><a href="#contact" className={``}>Contact</a></li>
-     </ul>
     </nav>
    </section>
 
