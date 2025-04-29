55a657243490ec9ea06f2cb2c786add28bafd30d
mubarak-codes <mubarakcodes@gmail.com>
2025-04-29 07:17:19 +0100

    bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 const queryClient = new QueryClient()
 
