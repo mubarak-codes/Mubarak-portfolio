f691a98bba7e2f3e4f04e81b28c28771972d1994
mubarak-codes <mubarakcodes@gmail.com>
2025-05-11 21:56:54 +0100

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,6 +13,8 @@ const Skills = ()=>{
      <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-js me3 fs9 vertical-center"></i>JavaScript</div>
      <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-bootstrap me3 fs9 vertical-center"></i>Bootstrap</div>
      <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-react me3 fs9 vertical-center"></i>React</div>
+     <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-js me3 fs9 vertical-center"></i>Typescript</div>
+     <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-angular me3 fs9 vertical-center"></i>Angular</div>
      <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-git-alt me3 fs9 vertical-center"></i>Git</div>
      <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-github me3 fs9 vertical-center"></i>Github</div>
      <div className=" py4 bg-gray10 px3 hover-grow hover-opacity75 transit"><i className="fab fa-html5 me3 fs9 vertical-center"></i>MkCSS</div>
