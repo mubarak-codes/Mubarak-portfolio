9cf3d8b559e4848d4572d7c86b17aaee54f09a32
mubarak-codes <mubarakcodes@gmail.com>
2025-05-22 17:53:35 +0100

    f

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = ({text, bg})=>{
   <>
    <section className=" mb5">
     <nav className={`navbar navbar-expand-sm ${bg} fixed-top py3 left0  right0 top0 `}>
-     <div className=" mx5 px10-sm px19-md py3 py1-sm flex items-center content-between w100">
+     <div className=" mx5 px10-sm px19-md py3 py1-sm flex items-center content-between w100 relative">
       {/*logo*/}
       <div className=" fw-bolder ff-rubik text-uppercase text-green fs5 ">
           Mubarak
       </div>
-      <i role="button" aria-controls="offcanvasExample" data-bs-target="#navbar" data-bs-toggle="collapse" className="fa fa-bars d-inline d-sm-none fs6 ms-auto"></i>
+      <i role="button" aria-controls="offcanvasExample" data-bs-target="#navbar" data-bs-toggle="collapse" className="fa fa-bars d-inline d-sm-none fs6 ms-auto absolute-xs right0"></i>
 
       <div id="navbar" className="collapse navbar-collapse">
-       <ul className="flex fs2 gap-3 navbar-nav py3 ms-auto">
+       <ul className="flex fs2 gap3 bg-dark navbar-nav py3 ms-auto absolute-xs left0 top50px shadow3">
         <li className="nav-item cursor-pointer hover-fade"><a href="#about" className={`link-inherit`}>About</a></li>
         <li className="nav-item cursor-pointer hover-fade"><a href="#projects" className={`link-inherit`}>Projects</a></li>
         <li className="nav-item cursor-pointer hover-fade"><a href="#experience" className={`link-inherit`}>Experience</a></li>
