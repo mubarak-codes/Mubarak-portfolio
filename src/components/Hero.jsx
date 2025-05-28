bfaa7c42e5e5a7f8c01876fec81b529ef7a486ea
mubarak-codes <mubarakcodes@gmail.com>
2025-05-28 16:56:59 +0100

    resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,7 @@ const Hero = ({bg, text, icon, setBg, setIcon, setText})=>{
            I'm <span className="text-green fw-semibold ">Mubarak Owosheni</span>, 18 years old, with 3+ years of experience. With expertise in making modern web apps.
           </div>
          <div  className="flex content-center content-start-sm my3" download>
-          <a href="files/Mubarak_Owosheni_Resume.pdf" className="bg-green b b-trans hover-bg-trans hover-b-green hover-text-green rounded-pill w30 fs2 py2 text-center text-snow fw-semibold opacity100 cursor-pointer link">Resume</a>
+          <a href="files/Mubarak_Resume.pdf" className="bg-green b b-trans hover-bg-trans hover-b-green hover-text-green rounded-pill w30 fs2 py2 text-center text-snow fw-semibold opacity100 cursor-pointer link">Resume</a>
 
          </div>
         </div>
