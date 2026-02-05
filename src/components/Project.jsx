d5e4f5c58a3ed9ff76bc15fe155181016c0e0fab
Mubarak <mubarakcodes@gmail.com>
2026-02-05 17:06:43 +0100

    photo

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,16 +13,13 @@ const Project = ({bg, text,})=>{
     <h2 className="text-center mb8 fs9 txt-bolder">Project</h2>
     <div className=" gap-4 d-grid gr2-sm">
      {projects.map((project)=>(
-      <div className={`b b-gray50 rounded-4 flex p5  ${text}`} key={project.id}>
-       <div className="h350px">
-        <div className="mb7 flex h80 w100 content-center h40-md h100-lg">
-         <img src={project.image} className="w100 br2 b h70 obj-cover items-start" />
-        </div>
+      <div className={`b b-gray50 rounded-4 flex p5  ${text} d-col`} key={project.id}>
+         <img src={project.image} className="w100 br2 b obj-cover items-start mb5" />
         <h4 className="ff-montserrat-bold mb2 fs3">{project.title}</h4>
         <div className="mb2 fs2 opacity75">{project.description.substring(0, 80)}...</div>
-        <a href={project.link} className={`fs1 text-green b b-green opacity75 click-fade click-grow link cursor-pointer p1 px2 br2`} target="_blank">Visit Website <i className="bi bi-arrow-up-right vertical-center"></i> </a> <hr/>
+      <div>
+        <a href={project.link} className={`fs1 text-green b b-green opacity75 click-fade click-grow link cursor-pointer p1 px2 br2 me20`} target="_blank">Visit Website <i className="bi bi-arrow-up-right vertical-center"></i> </a></div> <hr/>
         <div className="mt2 fs2 opacity75  ">{project.stack}</div>
-       </div>
       </div>
      ))}
     </div>
