76d404ff71a9d5d072d61dee4b6e75b3fdc15d4c
Mubarak <mubarakcodes@gmail.com>
2026-02-13 06:56:41 +0100

    test

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,17 +3,17 @@ const Services = () => {
   <>
    <section id="services" className="mt10 ps10-sm ps19-md px4">
     <h2 className="mb6 fs9 txt-bolder txt-center txt-start-sm">Services</h2>
-    <div className="flex d-col nowrap gap2">
+    <div className="flex d-col nowrap-sm gap2">
      {/*front end Developer*/}
       <div className=" fs4 mb3 bg-gray10 p4 br2">
-       <h3 className='fs5 txt-bold'>Front-End Developer</h3>
-       <div>Crafting responsive, interactive web experiences</div>
+       <h3 className='fs4 txt-bold'>Front-End Developer</h3>
+       <div className="fs2">Crafting responsive, interactive web experiences</div>
   </div>
 
     {/*back end Developer*/}
       <div className=" fs4 mb3 bg-gray10 p4 br2">
-      <h3 className="fs5 txt-bold">Back-End Developer</h3>
-      <div>Building secure APIs, servers & database systems</div>
+      <h3 className="fs4 txt-bold">Back-End Developer</h3>
+      <div className="fs2">Building secure APIs, servers & database systems</div>
   </div>
     </div>
    </section>
