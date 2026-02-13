28da4621f9f22c4d4b48d5a58e82e3a096e330fa
Mubarak <mubarakcodes@gmail.com>
2026-02-13 07:37:49 +0100

    service shadow

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,13 +5,13 @@ const Services = () => {
     <h2 className="mb6 fs9 txt-bolder txt-center txt-start-sm ff-montserrat-bold">Services</h2>
     <div className="flex d-col nowrap-sm gap2">
      {/*front end Developer*/}
-      <div className=" fs4 mb3 bg-gray10 p4 br2">
+      <div className=" fs4 mb3 bg-gray10 p4 hover-grow hover-shadow3">
        <h3 className='fs3 txt-bold'>Front-End Developer</h3>
        <div className="fs1">Crafting responsive, interactive web experiences</div>
   </div>
 
     {/*back end Developer*/}
-      <div className=" fs4 mb3 bg-gray10 p4 br2">
+      <div className=" fs4 mb3 bg-gray10 p4 hover-grow hover-shadow3">
       <h3 className="fs3 txt-bold">Back-End Developer</h3>
       <div className="fs1">Building secure APIs, servers & database systems</div>
   </div>
