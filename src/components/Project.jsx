b3c4edf4534fe559d0ef48528d6a9e90ec3297df
Mubarak <mubarakcodes@gmail.com>
2026-02-13 07:31:06 +0100

    experience

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,16 +10,16 @@ const Project = ({bg, text,})=>{
  return(
   <>
    <section className="mt12 px5 px10-sm px19-md" id="projects">
-    <h2 className="text-center mb8 fs9 txt-bolder">Project</h2>
+    <h2 className="text-center mb8 fs9 txt-bolder ff-montserrat-bold">My Projects</h2>
     <div className=" gap-4 d-grid gr2-sm">
      {projects.map((project)=>(
       <div className={`b b-gray50 rounded-4 flex p5  ${text} d-col`} key={project.id}>
          <img src={project.image} className="w100 br2 b obj-cover items-start mb5" />
         <h4 className="ff-montserrat-bold mb2 fs3">{project.title}</h4>
-        <div className="mb2 fs2 opacity75">{project.description.substring(0, 80)}...</div>
+        <div className="mb2 fs1 opacity75">{project.description.substring(0, 80)}...</div>
       <div>
-        <a href={project.link} className={`fs1 text-green b b-green opacity75 click-fade click-grow link cursor-pointer p1 px2 br2 me20`} target="_blank">Visit Website <i className="bi bi-arrow-up-right vertical-center"></i> </a></div> <hr/>
-        <div className="mt2 fs2 opacity75  ">{project.stack}</div>
+        <a href={project.link} className={`fs2 text-green b b-green opacity75 click-fade click-grow link cursor-pointer p1 px2 br2 me20`} target="_blank">Visit Website <i className="bi bi-arrow-up-right vertical-center"></i> </a></div> <hr/>
+        <div className="mt2 fs1 opacity75  ">{project.stack}</div>
       </div>
      ))}
     </div>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,18 +2,18 @@ const Services = () => {
  return (
   <>
    <section id="services" className="mt10 ps10-sm ps19-md px4">
-    <h2 className="mb6 fs9 txt-bolder txt-center txt-start-sm">Services</h2>
+    <h2 className="mb6 fs9 txt-bolder txt-center txt-start-sm ff-montserrat-bold">Services</h2>
     <div className="flex d-col nowrap-sm gap2">
      {/*front end Developer*/}
       <div className=" fs4 mb3 bg-gray10 p4 br2">
-       <h3 className='fs4 txt-bold'>Front-End Developer</h3>
-       <div className="fs2">Crafting responsive, interactive web experiences</div>
+       <h3 className='fs3 txt-bold'>Front-End Developer</h3>
+       <div className="fs1">Crafting responsive, interactive web experiences</div>
   </div>
 
     {/*back end Developer*/}
       <div className=" fs4 mb3 bg-gray10 p4 br2">
-      <h3 className="fs4 txt-bold">Back-End Developer</h3>
-      <div className="fs2">Building secure APIs, servers & database systems</div>
+      <h3 className="fs3 txt-bold">Back-End Developer</h3>
+      <div className="fs1">Building secure APIs, servers & database systems</div>
   </div>
     </div>
    </section>
