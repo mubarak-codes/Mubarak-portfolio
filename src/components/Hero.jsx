b35f102547e420bcdaa7515b978202f27990f0bb
Mubarak <mubarakcodes@gmail.com>
2026-02-17 23:22:46 +0100

    change design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = ({bg, text})=>{
      <section className=" mt16 px5 px10-sm px19-md"> 
       <section className=" flex mt4 items-center d-row-reverse-sm d-col-reverse pt4">
         <div className="profile flex items-start content-center py4">
-         <img src="img/MubarakResume.png" className=" w80 w60-sm -me5-sm  ms-sm-auto mt5" style={{marginLeft:'27px'}}/>
+         <img src="img/MubarakResume.png" className=" w50 w40-sm -me5-sm  ms-sm-auto mt5"/>
                  </div>
         {/*profile info*/}
         <div className=" content-center flex py-3 content-start-sm gap1 d-col ">
@@ -16,13 +16,13 @@ const Hero = ({bg, text})=>{
            <div className="txt-bolder text-3xl nowrap ff-montserrat-bold text-center sm:text-start">
             Software Developer
            </div>
-   <div className="text-center sm:text-start opacity-75">
-    Hi, I'm <span className="font-black opacity-100"> Mubarak Owosheni</span>. I specialize in building dynamic, scalable, and high- performnance web and mobile applications that deliver seamless user experiences with a strong emphasis on functionality, adaptability, and user engagement.
+   <div className="flex justify-center sm:justify-start opacity-75">
+    Hi, I'm Mubarak Owosheni. I specialize in building dynamic, scalable, and high- performnance web and mobile applications that deliver seamless user experiences with a strong emphasis on functionality, adaptability, and user engagement.
    </div>
           </div>
 
           {/*social icons*/}
-          <div className={`text-center text-sm-start text-sm ${text} mb3 flex gap-2 justify-center items-center opacity-75`}>
+          <div className={`text-center text-sm-start text-sm ${text} mb3 flex gap-2 justify-center items-center opacity-75 sm:justify-start`}>
             <a href="https://x.com/mubarak_codes" target="_blank">
                <i className={`bi bi-twitter-x ${text} click-grow click-fade bg-gray-500/25 p-2 rounded-full`}></i>
             </a>
