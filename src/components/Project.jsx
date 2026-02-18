491dcd8c4df5cd76a4add310773db1ff2a7d1af4
Mubarak <mubarakcodes@gmail.com>
2026-02-18 22:50:01 +0100

    test

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,11 +10,11 @@ const Project = ({bg, text,})=>{
  return(
   <>
    <section className="mt12 px5 px10-sm px19-md" id="projects">
-    <h2 className="text-center mb8 fs9 txt-bolder ff-montserrat-bold">My Projects</h2>
+    <h2 className="text-center mb-3 text-2xl opacity-75 txt-bolder ff-montserrat-bold">My Projects</h2>
     <div className=" gap-4 d-grid gr2-sm">
      {projects.map((project)=>(
-      <div className={`b b-gray50 rounded-4 flex p5  ${text} d-col`} key={project.id}>
-         <img src={project.image} className="w100 br2 b obj-cover items-start mb5" />
+      <div className={`flex p5  ${text} d-col`} key={project.id}>
+         <img src={project.image} className="w100 border obj-cover items-start mb5 p-2" />
         <h4 className="ff-montserrat-bold mb2 fs3">{project.title}</h4>
         <div className="mb2 fs1 opacity75">{project.description.substring(0, 80)}...</div>
       <div>
